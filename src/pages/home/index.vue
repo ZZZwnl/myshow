79137866a4f6a6b459f2876cd41c995bbce04dcc
ssh68 <610353134@qq.com>
2019-05-20 00:33:00 +0800

    通过计算属性完成楼层数据填充

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -11,6 +11,21 @@
     <div class="menu">
       <img v-for="(item,index) in menus" :key="index" :src="item.image_src" alt>
     </div>
+    <!--楼层效果-->
+    <div v-for='(item,index) in getImgs' :key='index' class="floor">
+      <div class="floor-title">
+        <img :src="item.floor_title.image_src" alt="">
+      </div>
+      <div class="floor-content">
+        <div class="left">
+          <!-- <img :src="item.product_list[0].image_src" alt=""> -->
+          <img :src="item.leftImg.image_src" alt="">
+        </div>
+        <div class="right">
+          <img v-for='(img,i) in item.product_list' :key='i' :src="img.image_src" alt="">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,7 +42,8 @@ export default {
       // 该属性的作用：显示底部的小圆圈
       indicatorDots: true,
       imgUrls: [],
-      menus: []
+      menus: [],
+      floors:[]
     };
   },
   components: {
@@ -63,7 +79,7 @@ export default {
     let imgs = swiperRes.data.message.map(item => {
       return item.image_src
     })
-    that.imgUrls = imgs
+    this.imgUrls = imgs
 
     // 调用菜单接口
     // mpvue.request({
@@ -81,9 +97,31 @@ export default {
     // });
 
     let menuRes = await request('home/catitems')
-    that.menus = menuRes.data.message
+    this.menus = menuRes.data.message
+
+    // 获取楼层数据
+    let floorsRes = await request('home/floordata')
+    this.floors = floorsRes.data.message
+  },
+  computed: {
+    getImgs(){
+      // 加工楼层数据:必须最后返回加工之后的结果
+      // 计算属性依赖于data中的数据
+      return this.floors.map(item=>{
+        // 过滤出图片的后四项数据
+        let imgs = item.product_list.filter((img,index)=>{
+          return index > 0
+        })
+        // 添加一条数据保存左侧图片
+        item.leftImg = item.product_list[0]
+        // 覆盖默认的5张图片
+        item.product_list = imgs
+        // 把加工之后的数据返回
+        return item
+      })
+    }
   }
-};
+}
 </script>
 
 <style scoped>
@@ -97,5 +135,31 @@ export default {
 .menu img {
   width: 128rpx;
   height: 140rpx;
+}
+.floor {
+  margin-top: 20rpx;
+}
+.floor .floor-title img{
+  width: 750rpx;
+  height: 80rpx;
+}
+.floor .floor-content{
+  display: flex;
+  justify-content: space-around;
+}
+.floor .floor-content .left img{
+  width: 260rpx;
+  height: 370rpx;
+}
+.floor .floor-content .right{
+  flex: 1;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+.floor .floor-content .right img{
+  width: 232rpx;
+  height: 188rpx;
+  border-radius: 4rpx;
 }
 </style>
