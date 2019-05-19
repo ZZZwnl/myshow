f708729746adb92abf0f353423deab753ccf1b18
ssh68 <610353134@qq.com>
2019-05-19 09:42:36 +0800

    完成菜单布局

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -8,6 +8,12 @@
         <image :src="item" class="slide-image">
       </swiper-item>
     </swiper>
+    <div class="menu">
+      <img src="/static/images/user.png" alt>
+      <img src="/static/images/user.png" alt>
+      <img src="/static/images/user.png" alt>
+      <img src="/static/images/user.png" alt>
+    </div>
   </div>
 </template>
 
@@ -17,7 +23,7 @@ import SearchBar from "../../components/search";
 export default {
   data() {
     return {
-        // 该属性的作用：显示底部的小圆圈
+      // 该属性的作用：显示底部的小圆圈
       indicatorDots: true,
       imgUrls: [
         "https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640",
@@ -28,6 +34,24 @@ export default {
   },
   components: {
     "search-bar": SearchBar
+  },
+  created() {
+    let that = this;
+    // 调用后台接口，获取轮播图数据
+    mpvue.request({
+      url: "https://www.zhengzhicheng.cn/api/public/v1/home/swiperdata",
+      success: function(res) {
+        // console.log(res)
+        // 使用服务器返回的数据覆盖data中的数据
+        // that.imgUrls = res.data.message
+        // res.data.message本身是对象数组，但是我们只需要图片地址
+        let imgs = res.data.message.map(item => {
+          return item.image_src;
+        });
+        // 更新图片数据
+        that.imgUrls = imgs;
+      }
+    });
   }
 };
 </script>
@@ -35,5 +59,13 @@ export default {
 <style scoped>
 .slide-image {
   width: 750rpx;
+}
+.menu {
+  display: flex;
+  justify-content: space-around;
+}
+.menu img {
+  width: 128rpx;
+  height: 140rpx;
 }
 </style>
